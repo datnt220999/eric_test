bb0a01f0a4ae1850331522ac25913d8b48338bce
TIEN DAT DEV <datnt220999@gmail.com>
2024-12-17 14:36:52 +0700

    refactory

--- eric_test/app/helpers/Const.php
+++ eric_test/app/helpers/Const.php
@@ -1,3 +1,7 @@
 <?php
 //app/helpers/Const.php
 define('KEY',"ADSADAXSASAFSAFSSACAxcdcasdcssacas");
+define('HOST',"localhost");
+define('DBNAME',"eric_test");
+define('USERNAME',"root");
+define('PASSWORD',"");
